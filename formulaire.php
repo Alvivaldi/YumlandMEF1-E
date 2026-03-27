33e182e57f9a1545e7ded2926c91248f0c831e36
Charmelle <nanacharmelle77@gmail.com>
2026-03-27 14:31:32 +0100

    test

--- formulaire.php
+++ formulaire.php
@@ -19,7 +19,7 @@
 <body>
     <nav class="navbar">
         <div class="logo">
-            <img src="../images/logo.png" alt="Logo" />
+            <img src="images/logo.png" alt="Logo" />
         </div>
         <ul class="nav-links">
             <li><a href="index.php">Accueil</a></li>
