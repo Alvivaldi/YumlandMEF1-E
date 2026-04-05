4a8904bda45ad83329d28789ad4a727769001f82
Alvi <lambert.alexisperso@gmail.com>
2026-04-05 13:12:17 +0200

    correction inscription

--- inscription.php
+++ inscription.php
@@ -6,7 +6,7 @@ $message = ""; // Pour afficher des erreurs ou succès à l'utilisateur
 
 // Vérifie si le formulaire a été envoyé
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // 1. Récupération des données du formulaire
+    // 1. Récupération des données du formulaire (en utilisant les BONS noms)
     $email = $_POST['email'] ?? '';
     $password = $_POST['password'] ?? '';
     $nom = $_POST['nom'] ?? '';
@@ -16,11 +16,12 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
     // 2. Chargement des utilisateurs existants
     $utilisateurs = lireJSON('donnees/utilisateurs.json');
+    if (!is_array($utilisateurs)) { $utilisateurs = []; }
 
     // 3. Vérification si l'email existe déjà
     $existe = false;
     foreach ($utilisateurs as $user) {
-        if ($user['login'] === $email) {
+        if (isset($user['login']) && $user['login'] === $email) {
             $existe = true;
             break;
         }
@@ -38,7 +39,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             "nom" => $nom,
             "prenom" => $prenom,
             "adresse" => $adresse,
-            "telephone" => $tel,
+            "telephone" => $telephone, // CORRECTION : C'était $tel avant !
             "date_inscription" => date("Y-m-d")
         ];
 
@@ -62,14 +63,12 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     <title>Inscription</title>
     <link rel="stylesheet" href="css/formulaire.css">
 
-
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Mogra&display=swap" />
 
     <link rel="preconnect" href="https://fonts.googleapis.com" />
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
     <link href="https://fonts.googleapis.com/css2?family=Chewy&family=Mogra&display=swap" rel="stylesheet" />
-
-
 </head>
 
 <body>
@@ -84,47 +83,45 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         <h1>Inscription</h1>
 
         <?php if ($message != ""): ?>
-            <p style="color: red;"><?php echo $message; ?></p>
+            <p style="color: red; text-align: center; font-weight: bold;"><?php echo $message; ?></p>
         <?php endif; ?>
 
         <form action="inscription.php" method="post">
             <div class="input-box">
-                <input type="text" name="nom" placeholder="Nom">
+                <input type="text" name="nom" placeholder="Nom" required>
                 <i class="fa-solid fa-user"></i>
             </div>
             <div class="input-box">
-                <input type="text" name="prenom" placeholder="Prénom">
+                <input type="text" name="prenom" placeholder="Prénom" required>
                 <i class="fa-solid fa-user"></i>
             </div>
             <div class="input-box">
-                <input type="text" name="email" placeholder="e-mail">
+                <input type="email" name="email" placeholder="e-mail" required>
                 <i class="fa-solid fa-envelope"></i>
             </div>
             <div class="input-box">
-                <input type="text" name="address" placeholder="Adresse de livraison">
+                <input type="text" name="adresse" placeholder="Adresse de livraison" required>
                 <i class="fa-solid fa-location-dot"></i>
             </div>
             <div class="input-box">
-                <input type="text" name="phone" placeholder="Numéro de téléphone">
+                <input type="text" name="telephone" placeholder="Numéro de téléphone" required>
                 <i class="fa-solid fa-phone"></i>
             </div>
             <div class="input-box">
-                <input type="password" name="password" placeholder="Mot de passe">
+                <input type="password" name="password" placeholder="Mot de passe" required>
                 <i class="fa-solid fa-lock"></i>
             </div>
             <div class="remember-forgot">
                 <label>
-                    <input type="checkbox"> J'accepte les termes et conditions
+                    <input type="checkbox" required> J'accepte les termes et conditions
                 </label>
             </div>
             <button type="submit" class="login-btn">S'inscrire</button>
-            <div class=" register-link">
+            <div class="register-link">
                 <p>Déjà un compte ? <a href="formulaire.php">Se connecter</a></p>
             </div>
-
         </form>
     </section>
 
 </body>
-
 </html>
